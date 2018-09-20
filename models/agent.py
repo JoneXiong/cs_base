a324c0d5cddbff64bbbd860ba4a019832f5c1f21
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-09-20 11:57:30 +0800

    增加坐席组获取可用坐席及从可用坐席中选取一个坐席的逻辑实现

--- models/agent.py
+++ models/agent.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import random
 
 from odoo import models, fields, api
 
@@ -14,6 +15,22 @@ class CsCroup(models.Model):
         ], string=' 状态', default='normal')
     agent_ids = fields.Many2many('cs.agent', relation='cs_agent_group_rel', string='坐席成员')
 
+    @api.multi
+    def get_available_agents(self):
+        agents = []
+        for group in self:
+            agents += group.agent_ids
+        agents = [agent for agent in agents if agent.status=='on']
+        return agents
+
+    @api.multi
+    def choose_agent(self):
+        available_agents = self.get_available_agents()
+        if available_agents:
+            return random.choice(available_agents)
+        else:
+            return None
+
 
 class Agent(models.Model):
     _name = 'cs.agent'
@@ -24,4 +41,8 @@ class Agent(models.Model):
     user_id = fields.Many2one('res.users', string=u'坐席用户')
     group_ids = fields.Many2many('cs.group', relation='cs_agent_group_rel', string=u'所属客服组')
     remark = fields.Text(string='备注')
+    status = fields.Selection([
+        ('off', '离线'),
+        ('on', '在线'),
+        ], string=' 状态', default='off')
 
