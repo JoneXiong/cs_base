5823d8834123c1bcbdf6ca7fecd205f79d6eb53e
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2019-02-22 15:40:10 +0800

    当坐席设为离线时，对应的用户不可用于客服

--- models/res_users.py
+++ models/res_users.py
@@ -7,3 +7,10 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     agent_ids = fields.One2many('cs.agent', 'user_id', '关联的坐席')
+
+    def is_available(self):
+        if self.agent_ids:
+            agent = self.agent_ids[0]
+            if agent.status=='off':
+                return False
+        return self.im_status=="online"
